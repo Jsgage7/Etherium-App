ca82c0e2fe1c61b24d4a78887e3e698543a7a4c7
Jared Gage <78756562+Jsgage7@users.noreply.github.com>
2021-09-01 20:59:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Etherium-App
+ 
---
+++ contracts/wavecounter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract wavecounter{
+    constructor(){
+        console.log("Hello, I'm Jarvic (Just A Rather Very Intelligent Contract)");
+    }
+}
